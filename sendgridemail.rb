0b512423760c464d294b4ffd65de8c718d1f0cf8
Stephon <eosteph21@gmail.com>
2014-06-21 17:34:20 -0400

    new from person

--- sendgridemail.rb
+++ sendgridemail.rb
@@ -5,7 +5,7 @@ response = HTTParty.post url, :body => {
   "api_user" => "eosteph21",
   "api_key" => "XXXXXX",
   "to" => "eosteph21@gmail.com",
-  "from" => "stephonmccoy@greetmeet.net",
+  "from" => "questions@greetmeet.net",
   "subject" => "Check Check",
   "text" => "Congrats! You've sent your first email with SendGrid from your new account."
 }
